a513b2da0c60ea5ed5d8a682ae1cdcee2470b79c
gd-stones <ngducviet2105@gmail.com>
2024-03-23 11:31:24 +0700

    redesign tool

--- LDT_v2.Designer.cs
+++ LDT_v2.Designer.cs
@@ -33,6 +33,7 @@
             Map_Tool = new Panel();
             Item_List_View = new Panel();
             ButtonExport = new Button();
+            Item_Content = new Panel();
             SuspendLayout();
             // 
             // imageList1
@@ -43,10 +44,12 @@
             // 
             // Map_Tool
             // 
+            Map_Tool.AutoScroll = true;
+            Map_Tool.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             Map_Tool.BorderStyle = BorderStyle.FixedSingle;
             Map_Tool.Location = new Point(0, 0);
             Map_Tool.Name = "Map_Tool";
-            Map_Tool.Size = new Size(1920, 621);
+            Map_Tool.Size = new Size(1440, 1248);
             Map_Tool.TabIndex = 0;
             Map_Tool.Paint += Map_Tool_Paint;
             // 
@@ -54,9 +57,9 @@
             // 
             Item_List_View.AutoScroll = true;
             Item_List_View.BorderStyle = BorderStyle.FixedSingle;
-            Item_List_View.Location = new Point(1, 680);
+            Item_List_View.Location = new Point(1456, 420);
             Item_List_View.Name = "Item_List_View";
-            Item_List_View.Size = new Size(600, 320);
+            Item_List_View.Size = new Size(450, 550);
             Item_List_View.TabIndex = 1;
             Item_List_View.Paint += Item_List_View_Paint;
             // 
@@ -64,7 +67,7 @@
             // 
             ButtonExport.BackColor = Color.FromArgb(255, 224, 192);
             ButtonExport.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            ButtonExport.Location = new Point(5, 634);
+            ButtonExport.Location = new Point(1456, 993);
             ButtonExport.Name = "ButtonExport";
             ButtonExport.Size = new Size(110, 32);
             ButtonExport.TabIndex = 2;
@@ -72,17 +75,28 @@
             ButtonExport.UseVisualStyleBackColor = false;
             ButtonExport.Click += ButtonExport_Click;
             // 
+            // Item_Content
+            // 
+            Item_Content.AutoSize = true;
+            Item_Content.BorderStyle = BorderStyle.FixedSingle;
+            Item_Content.Location = new Point(1456, 0);
+            Item_Content.Name = "Item_Content";
+            Item_Content.Size = new Size(450, 400);
+            Item_Content.TabIndex = 3;
+            // 
             // LDT_v2
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1924, 1061);
+            Controls.Add(Item_Content);
             Controls.Add(ButtonExport);
             Controls.Add(Item_List_View);
             Controls.Add(Map_Tool);
             Name = "LDT_v2";
             Text = "LDT_v2";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -91,5 +105,6 @@
         private Panel Map_Tool;
         private Panel Item_List_View;
         private Button ButtonExport;
+        private Panel Item_Content;
     }
 }
--- LDT_v2.cs
+++ LDT_v2.cs
@@ -13,17 +13,19 @@ namespace LevelDesignTool
 {
     public partial class LDT_v2 : Form
     {
-        private const int gridSize = 16; // Size of each square in the grid
+        private const int gridSize = 16;
         private List<Item> items = new List<Item>();
         private int selectedItemType = 0;
 
         public LDT_v2()
         {
             InitializeComponent();
-            Map_Tool.Paint += Map_Tool_DrawGrid;
+            //Map_Tool.Paint += Map_Tool_DrawGrid;
             Map_Tool.AllowDrop = true;
             Map_Tool.MouseClick += Map_Tool_MouseClick;
             Map_Tool.AutoScrollMinSize = new Size(3200, 1248);
+            InitializeMapToolPanel();
+            //Map_Tool.MaximumSize = new Size(3200, 1248);
 
             ReadItemFromFile();
             DisplayItems();
@@ -31,6 +33,19 @@ namespace LevelDesignTool
             {
                 item.ItemClicked += Item_ItemClicked;
             }
+        }
+
+        private void InitializeMapToolPanel()
+        {
+            Map_Tool.Width = 3200;
+            Map_Tool.Height = 1248;
+            Map_Tool.AutoScroll = true; // Allow scrolling if the panel is larger than the container
+
+            // Reduce flickering by enabling double buffering
+            Map_Tool.GetType().GetProperty("DoubleBuffered", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).SetValue(Map_Tool, true);
+
+            // Subscribe to the Paint event of Map_Tool
+            Map_Tool.Paint += new PaintEventHandler(Map_Tool_Paint);
         }
 
         private void Map_Tool_MouseClick(object sender, MouseEventArgs e)
@@ -96,11 +111,46 @@ namespace LevelDesignTool
 
         private void Map_Tool_Paint(object sender, PaintEventArgs e)
         {
+            // The size of the grid
+            int gridWidth = 3200;
+            int gridHeight = 1248;
+            int cellSize = 32;
 
+            Graphics g = e.Graphics;
+            Pen gridPen = new Pen(Color.Black);
+
+            // Create a red pen for the first lines
+            Pen firstLinePen = new Pen(Color.Red);
+
+            // Adjust for the current scroll position
+            Point scrollPosition = new Point(-Map_Tool.AutoScrollPosition.X, -Map_Tool.AutoScrollPosition.Y);
+
+            // Draw the first horizontal line in red
+            g.DrawLine(firstLinePen, scrollPosition.X, scrollPosition.Y + gridHeight, scrollPosition.X + gridWidth, scrollPosition.Y + gridHeight);
+
+            // Draw the first vertical line in red
+            g.DrawLine(firstLinePen, scrollPosition.X, scrollPosition.Y, scrollPosition.X, scrollPosition.Y + gridHeight);
+
+            // Draw the rest of the horizontal grid lines
+            for (int y = gridHeight - cellSize; y >= 0; y -= cellSize)
+            {
+                g.DrawLine(gridPen, scrollPosition.X, scrollPosition.Y + y, scrollPosition.X + gridWidth, scrollPosition.Y + y);
+            }
+
+            // Draw the rest of the vertical grid lines
+            for (int x = cellSize; x <= gridWidth; x += cellSize)
+            {
+                g.DrawLine(gridPen, scrollPosition.X + x, scrollPosition.Y, scrollPosition.X + x, scrollPosition.Y + gridHeight);
+            }
+
+            // Dispose of the pens after using
+            gridPen.Dispose();
+            firstLinePen.Dispose();
         }
 
         private void Map_Tool_DrawGrid(object sender, PaintEventArgs e)
         {
+            // sludion 1
             //Panel panel = (Panel)sender;
             //Graphics g = e.Graphics;
             //Pen pen = new Pen(Color.Black);
@@ -117,7 +167,9 @@ namespace LevelDesignTool
             //        g.DrawRectangle(pen, x, y, gridSize, gridSize);
             //    }
             //}
+            //pen.Dispose();
 
+            // slodion 2
             Panel panel = (Panel)sender;
             Graphics g = e.Graphics;
             Pen pen = new Pen(Color.Black);
@@ -139,9 +191,41 @@ namespace LevelDesignTool
                 int x = j * gridSize;
                 g.DrawLine(pen, x, 0, x, numRows * gridSize); // Draw line from top to bottom
             }
-
-            // Cleanup: Dispose of the pen when done to free resources
             pen.Dispose();
+
+
+            // slodion 3
+            //Panel panel = (Panel)sender;
+            //Graphics g = e.Graphics;
+
+            //// Calculate number of rows and columns based on your panel size
+            //int numRows = 1248 / gridSize;
+            //int numCols = 3200 / gridSize;
+
+            //g.Clear(panel.BackColor); // Clear the panel if necessary
+
+            //// Set up the transform for the graphics object to flip and translate
+            //g.ScaleTransform(1, -1); // Flip the y-axis
+            //g.TranslateTransform(0, -1248); // Translate to bottom-left
+
+            //Pen pen = new Pen(Color.Black);
+
+            //// Draw the vertical grid lines
+            //for (int i = 0; i <= numCols; i++)
+            //{
+            //    int x = i * gridSize;
+            //    g.DrawLine(pen, x, 0, x, numRows * gridSize); // Draw line from bottom to top
+            //}
+
+            //// Draw the horizontal grid lines
+            //for (int j = 0; j <= numRows; j++)
+            //{
+            //    int y = j * gridSize;
+            //    g.DrawLine(pen, 0, y, numCols * gridSize, y); // Draw line from left to right
+            //}
+
+            //// Dispose of the pen to free up resources
+            //pen.Dispose();
         }
 
         private void Item_List_View_Paint(object sender, PaintEventArgs e)
@@ -191,10 +275,55 @@ namespace LevelDesignTool
             }
         }
 
+        //private void ButtonExport_Click(object sender, EventArgs e)
+        //{
+        //    // Create a new list to store the sorted PictureBox controls
+        //    List<PictureBox> pictureBoxes = new List<PictureBox>();
+
+        //    // Gather all PictureBox controls from the Map_Tool panel
+        //    foreach (Control control in Map_Tool.Controls)
+        //    {
+        //        if (control is PictureBox)
+        //        {
+        //            pictureBoxes.Add((PictureBox)control);
+        //        }
+        //    }
+
+        //    // Sort the PictureBoxes by Top first and then Left positions
+        //    pictureBoxes.Sort((x, y) =>
+        //    {
+        //        int result = x.Top.CompareTo(y.Top);
+        //        return (result == 0) ? x.Left.CompareTo(y.Left) : result;
+        //    });
+
+        //    // Now pictureBoxes are sorted, you can export their locations
+        //    StringBuilder exportData = new StringBuilder();
+
+        //    foreach (PictureBox pb in pictureBoxes)
+        //    {
+        //        exportData.AppendLine($"PictureBox: {pb.Location.X}, {pb.Location.Y}");
+        //    }
+
+        //    string exportFilePath = @"C:\Users\Admin\Desktop\Game\Programming Language\C Sharp\LevelDesignTool\ExportedPositions.txt";
+
+        //    try
+        //    {
+        //        File.WriteAllText(exportFilePath, exportData.ToString());
+        //        MessageBox.Show($"Exported PictureBox positions to {exportFilePath}", "Export Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show($"Error exporting positions: {ex.Message}", "Export Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //}
+
         private void ButtonExport_Click(object sender, EventArgs e)
         {
             // Create a new list to store the sorted PictureBox controls
             List<PictureBox> pictureBoxes = new List<PictureBox>();
+
+            // The AutoScrollPosition refers to the current scroll location of the Map_Tool Panel
+            Point scrollPosition = new Point(-Map_Tool.AutoScrollPosition.X, -Map_Tool.AutoScrollPosition.Y);
 
             // Gather all PictureBox controls from the Map_Tool panel
             foreach (Control control in Map_Tool.Controls)
@@ -217,7 +346,9 @@ namespace LevelDesignTool
 
             foreach (PictureBox pb in pictureBoxes)
             {
-                exportData.AppendLine($"PictureBox: {pb.Location.X}, {pb.Location.Y}");
+                // Adjust the location based on the scroll position
+                var adjustedLocation = new Point(pb.Location.X + scrollPosition.X, pb.Location.Y + scrollPosition.Y);
+                exportData.AppendLine($"PictureBox: {adjustedLocation.X}, {adjustedLocation.Y}");
             }
 
             string exportFilePath = @"C:\Users\Admin\Desktop\Game\Programming Language\C Sharp\LevelDesignTool\ExportedPositions.txt";
